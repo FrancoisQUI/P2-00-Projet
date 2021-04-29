5c2498913ef7e4cb3aad977e0d88a0dd6a607b03
FrancoisQUI <fquivron@gmail.com>
2021-04-29 15:44:29 +0200

    search data with html class for one book, output in a dict

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-test_url = 'http://books.toscrape.com/catalogue' \
-      '/sorting-the-beef-from-the-bull-the-science-of-food-fraud-forensics_736' \
-      '/index.html'
+'''Url de test'''
+test_url = 'http://books.toscrape.com/catalogue/the-little-prince_72/index.html'
 
 
-def get_book_info(book_url):
-    response = requests.get(book_url)
-    if response.ok:
-        soup = BeautifulSoup(response.text, "html.parser")
-        title = soup.find('title').text
-        print(title)
+def get_book_data(book_url):
+    """
     
+    :rtype: dict
+    """
+    response = requests.get(book_url)
+    book_data = {}
+    if response.ok:
+        soup = BeautifulSoup(response.text, "lxml")
 
-get_book_info(test_url)
+        def get_the_data(cat):
+            book_data[cat] = soup.find(class_=str(cat)).text
+
+        get_the_data('active')
+        get_the_data('price_color')
+        get_the_data('')
+
+    return book_data
+
+
+print(get_book_data(test_url))
