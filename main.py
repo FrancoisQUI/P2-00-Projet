1d5e2c30da941a64e10fbadb923886efc4f2e38f
FrancoisQUI <fquivron@gmail.com>
2021-04-29 23:44:41 +0200

    Add function to scrape data from the table and scrape title, desc and cat

--- main.py
+++ main.py
@@ -1,28 +1,72 @@
+from pprint import pprint
+
 import requests
 from bs4 import BeautifulSoup
 
 '''Url de test'''
-test_url = 'http://books.toscrape.com/catalogue/the-little-prince_72/index.html'
+
+user_input = input("Url Page to scrape :\n    ")
+test_url = user_input if user_input != '' else "http://books.toscrape.com/catalogue/the-little-prince_72/index.html"
+
+# ie : http://books.toscrape.com/catalogue/the-little-prince_72/index.html
 
 
-def get_book_data(book_url):
+def clean_string(string: str):
+    string = string.replace("Â£", '')
+    return string
+
+
+def get_book_data(book_url: str):
     """
-    
     :rtype: dict
     """
     response = requests.get(book_url)
-    book_data = {}
+    book_data = {'product_page_url': book_url}
     if response.ok:
         soup = BeautifulSoup(response.text, "lxml")
 
-        def get_the_data(cat):
-            book_data[cat] = soup.find(class_=str(cat)).text
+        def get_data_from_table():
+            """
+            Read the table and push data in the book_data dict
+            """
+            table_lines = soup.findAll("tr")
+            for table_line in table_lines:
+                index = clean_string(table_line.find("th").get_text())
+                data = clean_string(table_line.find("td").get_text())
 
-        get_the_data('active')
-        get_the_data('price_color')
-        get_the_data('')
+                if index.lower() == "upc".lower():
+                    book_data["universal_ product_code"] = data
+                elif index.lower() == "Price (excl. tax)".lower():
+                    book_data["price_including_tax"] = data
+                elif index.lower() == "Price (incl. tax)".lower():
+                    book_data["price_excluding_tax"] = data
+                elif index.lower() == "Availability".lower():
+                    data = data.replace("In stock (", "")
+                    data = data.replace(" available)", "")
+                    book_data["number_available"] = data
+
+        def get_book_title():
+            title = soup.find("h1").get_text()
+            book_data["title"] = title
+
+        def get_book_description():
+            description = soup.find(id="product_description").find_next("p").get_text()
+            book_data['product_description'] = description
+
+        def get_book_category():
+            category_link = soup.select(".breadcrumb li:nth-child(3) a")
+            # TODO: Don't use CSS selector, but Bs methods instead
+            category = category_link[0].get_text()
+            book_data['category'] = category
+
+        get_book_title()
+        get_book_description()
+        get_book_category()
+        get_data_from_table()
+
+    # to scrape : product_description, review_rating, image_url
 
     return book_data
 
 
-print(get_book_data(test_url))
+pprint(get_book_data(test_url))
