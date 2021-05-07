b4807f57ab84e670757eba6edee478eb75a9f93a
FrancoisQUI <fquivron@gmail.com>
2021-05-07 13:33:46 +0200

    Refacto main.py, Get all book form given category, TO FIX: only the last book is on the {category}.csv

--- main.py
+++ main.py
@@ -8,36 +8,65 @@ OUTPUT_FILE_MAIN_DIR = "output_files"
 SITE_URL = "http://books.toscrape.com"
 TEST_FUNCTION = "Category"  # None, Book, Category
 
-if TEST_FUNCTION == "Book":
-    ctext("Test des fonctions livres en cours")
-    user_input = input("Url Page to scrape :\n     ")
-    TEST_URL = user_input if user_input != '' \
-        else "http://books.toscrape.com/catalogue/the-little-prince_72/index.html"
 
-    # ie : http://books.toscrape.com/catalogue/the-little-prince_72/index.html
+def scrape_a_book_and_hydrate_cvs(book_url: str,
+                                  site_url: str):
+    one_book = book_actions.get_book_data(book_url, site_url)
+    cvs_file_name = OUTPUT_FILE_MAIN_DIR + "/" + one_book['category'] + ".csv"
+    book_actions.book_data_to_csv(one_book, cvs_file_name)
 
-    a_book = book_actions.get_book_data(TEST_URL, SITE_URL)
-    pprint(a_book)
-    book_actions.book_data_to_csv(a_book,
-                                  OUTPUT_FILE_MAIN_DIR
-                                  + '/'
-                                  + a_book["universal_product_code"] + "-"
-                                  + str(a_book["title"]).replace(" ", "")
-                                  + ".csv")
 
-elif TEST_FUNCTION == "Category":
-    ctext("Test de Catégorie en cours.", "blue")
+def test_function(function_to_test):
+    if function_to_test == "Book":
+        ctext("Book's functions test :", 'blue')
+        user_input = input("Url Page to scrape :\n     ")
+        TEST_URL = user_input if user_input != '' \
+            else "http://books.toscrape.com/catalogue/the-little-prince_72/index.html"
 
-    user_input = input("Category to scrape :\n     ")
-    TESTED_CAT_URL = user_input if user_input != '' \
-        else"http://books.toscrape.com/catalogue/category/books/romance_8/index.html"
+        # ie : http://books.toscrape.com/catalogue/the-little-prince_72/index.html
 
-    ctext("Liste des livres de la page", "yellow")
-    category_books = cat_actions.get_books_url_from_category_url(TESTED_CAT_URL)
-    pprint(category_books)
-    ctext("Recherche d'une page next", "yellow")
-    next_page = cat_actions.get_category_next_page(TESTED_CAT_URL)
-    pprint(next_page)
+        a_book = book_actions.get_book_data(TEST_URL, SITE_URL)
+        pprint(a_book)
+        book_actions.book_data_to_csv(a_book,
+                                      OUTPUT_FILE_MAIN_DIR
+                                      + '/'
+                                      + a_book["universal_product_code"] + "-"
+                                      + str(a_book["title"]).replace(" ", "")
+                                      + ".csv")
 
-elif TEST_FUNCTION == "None":
-    ctext("WIP", "red")
+    elif function_to_test == "Category":
+        ctext("Categories functions test : ", "blue")
+
+        user_input = input("Category to scrape :\n     ")
+        TESTED_CAT_URL = user_input if user_input != '' \
+            else "http://books.toscrape.com/catalogue/category/books/romance_8/index.html"
+
+        page_to_scrape = TESTED_CAT_URL
+        pre_books_to_scrape = []
+        books_to_scrape = []
+
+        while True:
+            ctext("Category page book list", "yellow")
+            category_books = cat_actions.get_books_url_from_category_url(page_to_scrape)
+            pprint(category_books)
+            ctext("Next category page", "yellow")
+            page_to_scrape = cat_actions.get_category_next_page(page_to_scrape)
+            print(page_to_scrape)
+            pre_books_to_scrape.append(category_books)
+            if not page_to_scrape:
+                break
+
+        for book_list in pre_books_to_scrape:
+            for book in book_list:
+                books_to_scrape.append(book)
+
+        pprint(books_to_scrape)
+        ctext(f"there are {len(books_to_scrape)} books to scrape", 'green')
+
+        for book_to_scrape in books_to_scrape:
+            scrape_a_book_and_hydrate_cvs(book_to_scrape, SITE_URL)
+
+        ctext("Done")
+
+    elif function_to_test == "None":
+        ctext("WIP", "red")
--- modules/category_actions.py
+++ modules/category_actions.py
@@ -23,7 +23,7 @@ def get_books_url_from_category_url(category_url: str):
         books_url = get_page_books()
 
     else:
-        print("Erreur de requête")
+        print("Erreur de requête", response)
         return False
 
     return books_url
@@ -33,8 +33,8 @@ def get_category_next_page(category_url: str):
     response = requests.get(category_url)
     if response.ok:
         def get_category_url_name():
-            category_url_name = category_url.replace(
-                "index.html", "")
+            pos = category_url.rfind("/") + 1
+            category_url_name = category_url[:pos]
             return category_url_name
 
         soup = BeautifulSoup(response.content, "lxml")
@@ -47,5 +47,3 @@ def get_category_next_page(category_url: str):
 
         else:
             return False
-
-
